07f5db1a0e1e5a4b776460307178e25ec50326cb
kurota <whatEver@example.com>
2021-09-16 12:45:44 +0800

    [AutoPush] terminating commit to merge

--- git/weekly-push.sh
+++ git/weekly-push.sh
@@ -14,7 +14,6 @@ if [ "$1" = "dev" ]; then
     CONFLICTS=$(git ls-files -u | wc -l)
     if [ "$CONFLICTS" -gt 0 ] ; then
         echo "There is a merge conflict. Aborting"
-        git merge --abort
         exit 1
     fi
     git push
