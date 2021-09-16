24818f78289355d0ab0efc8030474217d37a1d53
kurota <whatEver@example.com>
2021-09-16 15:07:32 +0800

    [GITTOOLS] terminating commit to merge

--- git-tools/only-one-commit-branch.sh
+++ git-tools/only-one-commit-branch.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 current_branch_name=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-commit=$(git log release..GITTOOLS  --oneline | tail -1)
+last_commit=$(git log release..GITTOOLS  --oneline | tail -1)
+last_commit=${commit:0:7}
+git cherry   release ${current_branch_name}
+# sed -i "'s/^pick ce5efdb /edit ce5efdb /;/^pick ce6efdb /d'"
 
-GIT_SEQUENCE_EDITOR=true git rebase -i --autosquash c6c4526~1
+GIT_SEQUENCE_EDITOR=true git rebase -i --autosquash release~1
